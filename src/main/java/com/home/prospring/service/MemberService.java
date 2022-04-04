76c06068fbf6489a1f17761e587f820973bb6b04
fpem3309 <alsrhks2154@gmail.com>
2022-04-05 06:06:24 +0900

    repository

---
+++ src/main/java/com/home/prospring/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.home.prospring.service;
+
+import com.home.prospring.domain.Member;
+import com.home.prospring.repostory.MemberRepository;
+import com.home.prospring.repostory.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+        //같은 이름 중복 방지
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+    */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
