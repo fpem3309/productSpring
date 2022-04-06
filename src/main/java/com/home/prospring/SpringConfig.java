ed1b2a5393c1435c13c8840a8a9919a894ad2a77
fpem3309 <alsrhks2154@gmail.com>
2022-04-06 23:03:49 +0900

    member submit form_post & redirect root

--- src/main/java/com/home/prospring/SpringConfig.java
+++ src/main/java/com/home/prospring/SpringConfig.java
@@ -6,16 +6,16 @@ import com.home.prospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SpringConfig {
-
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
-}
+//@Configuration
+//public class SpringConfig {
+//
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+//}
--- src/main/java/com/home/prospring/controller/MemberController.java
+++ src/main/java/com/home/prospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.home.prospring.controller;
 
+import com.home.prospring.domain.Member;
 import com.home.prospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired  //Spring 컨테이너에서 memberService를 가져옴, MemberService에서 @Service 어노테이션을 해야함 ( DI )
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
