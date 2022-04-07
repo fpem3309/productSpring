4b7f52c0c08a0655d3e32c2670c6247b62ca6585
fpem3309 <alsrhks2154@gmail.com>
2022-04-07 10:43:32 +0900

    mysql connect User table_ save, findAll JdbcMemberRepository

--- src/main/java/com/home/prospring/SpringConfig.java
+++ src/main/java/com/home/prospring/SpringConfig.java
@@ -1,21 +1,33 @@
 package com.home.prospring;
 
+import com.home.prospring.repostory.JdbcMemberRepository;
 import com.home.prospring.repostory.MemberRepository;
 import com.home.prospring.repostory.MemoryMemberRepository;
 import com.home.prospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class SpringConfig {
-//
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
 //    @Bean
 //    public MemberService memberService(){
 //        return new MemberService(memberRepository());
 //    }
-//
-//    @Bean
-//    public MemberRepository memberRepository(){
+
+    @Bean
+    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-//    }
-//}
+        return new JdbcMemberRepository(dataSource);
+    }
+}
